dddf02585df8a93601da81f3e64b11801b2d757d
Amber Williams <awilliams1234@live.com>
2020-02-27 02:08:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,11 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition = "new"
   attr_reader :brand
   def initialize(brand)
     @brand = brand
   end 
   def cobble
-    if new 
     puts "Your shoe is as good as new!"
-  else
-    puts "tattered"
   end 
 end  
