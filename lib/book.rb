092189bec8bc88a503ec4716bff2a22424f848c4
Amber Williams <awilliams1234@live.com>
2020-02-27 01:53:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,9 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title 
-  #def initialize(title)
-   # @title = title 
-  #end 
+  def initialize(title)
+    @title = title 
+  end 
   #def title
    # @title 
   #end 
